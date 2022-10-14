9b53a8bd98b62e49f8de3fdbe21ec0ac4fb4c8f7
Tianxinr <115793309+Tianxinr@users.noreply.github.com>
2022-10-14 23:25:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,51 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxec3a66efe18382c0 ',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'ba84d54f023ac3273abf708228f2562f',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '新疆',
+  CITY: '乌鲁木齐',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '亲爱的小丁',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oXgd_6dBuRQqEp7OGBWHd3pzESF0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: ' 	Zz79BSkz5m6lCcqFNgFz_sKlaxPJSZfX6ZnjcKtbRNQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-02',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*03-02', name: '宝宝', year: '2000', date: '03-02',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '恋爱', name: '相识纪念日', year: '2021', date: '07-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2021-07-25' },
+        
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Zz79BSkz5m6lCcqFNgFz_sKlaxPJSZfX6ZnjcKtbRNQ',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '悦悦',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oXgd_6VtKVGE_eGRmVyCBht27UqQ',
     }
   ],
 
